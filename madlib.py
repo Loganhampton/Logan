043e84374c59f1d432a454f8f801911580b9f62c
Logan Hampton <logthampton@gmail.com>
2018-06-19 18:23:53 +0000

    second commit

---
+++ madlib.py
@@ -0,0 +1,6 @@
+food = raw_input("Provide favorite food")
+resturant  = raw_input("Provide favorite resturant")
+resturant = raw_input("Provide favorite resturant")
+artist = raw_input("Provide favorite song writer")
+Book = raw_input("Provide favorite book")
+madlib = raw_input("")
---
+++ shoppinglist.py
@@ -0,0 +1,3 @@
+myshopping_list = ["milk", "nuts", "ice cream", "Far cry 5"]
+for element in myshopping_list:
+    print(element)
